c39ca52dc7711d82662abbe6e521cf924adf9cda
skivuha <skivuha@hotmail.com>
2015-04-15 02:44:10 -0700

    Update config.php

--- config.php
+++ config.php
@@ -5,28 +5,28 @@ define('TEMPLATE', "templates/html/");
 define('LANG', "templates/lang/");
 
 //home
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'booker');
-define('DB_PASS', '');
-define('DB_LOGIN', 'root');
+//define('DB_HOST', 'localhost');
+//define('DB_NAME', 'booker');
+//define('DB_PASS', '');
+//define('DB_LOGIN', 'root');
 
-define('PATH', '/');
+//define('PATH', '/');
 
-define('CONTROLLER', '0');
-define('ACTION', '1');
-define('PARAM', '2');
+//define('CONTROLLER', '0');
+//define('ACTION', '1');
+//define('PARAM', '2');
 
 //work
-//define('DB_HOST', 'localhost');
-//define('DB_LOGIN', 'user2');
-//define('DB_NAME', 'user2');
-//define('DB_PASS','tuser2');
+define('DB_HOST', 'localhost');
+define('DB_LOGIN', 'user2');
+define('DB_NAME', 'user2');
+define('DB_PASS','tuser2');
 
-//define('PATH', '/~user2/PHP/shop/');
+define('PATH', '/~user2/PHP/shop/');
 
-//define('CONTROLLER', '3');
-//define('ACTION', '4');
-//define('PARAM', '5');
+define('CONTROLLER', '3');
+define('ACTION', '4');
+define('PARAM', '5');
 
 
 
